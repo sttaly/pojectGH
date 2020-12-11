7c504543d09f3ad7903538f5a2dae80d2db79a2a
sttaly <taly2598@gmail.com>
2020-12-11 09:44:06 -0500

    archivo sttaly

---
+++ sttaly_prueba.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+
+    <link rel="stylesheet" href="css/sttaly.css" type="text/css">
+</head>
+<body>
+<div>
+    <div class="container formulario">
+        <div class="row">
+            <form>
+                Nombre:<input type="tel"  placeholder="Escribe tu nombre aquí." /> <br><br>
+                Apellido:<input type="tel"  placeholder="Escribe tu apellido aquí." /> <br><br>
+                Correo:<input type="email" placeholder="Escribe tu correo aquí."/> <br><br>
+                Teléfono:<input type="tel"  placeholder="Escribe tu telefono aquí."  /> <br><br>
+                Número:<input type="tel"  placeholder="Escribe tu numero aquí."  /> <br><br>
+
+                <button>Agregar</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+    
+    
+</body>
+</html>
